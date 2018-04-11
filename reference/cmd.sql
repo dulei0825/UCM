ce1ada64d23161eae04ddac81cd2d9e9154e8de7
dulei0825 <6441556+dulei0825@users.noreply.github.com>
2018-04-12 01:16:07 +0800

    initial

--- reference/cmd.sql
+++
@@ -1,4 +0,0 @@
-insert into sn values (NULL, '212068', 4, 'HDS/ORIENTAL CABLE NETWORK', 'NO.2860 JIN KE RD, ZHANGJIANG', 'XU QIANJIA 13764911217', '', '', '', '00200002', '47477', '')
-insert into sr values (NULL, '47687443', '00295006', '', 41, '2018-04-10 13:13', 'HDD01-10', '');
-insert into ac values (NULL, 32, '2018-04-11', '14:00', '1小时', '', '', 'REPLACE HDD01-10', 'BREAK-FIX', '无报告，用户离开现场');
-insert into hw values (NULL, 32, 12, '355C1NW420GQ3C', 11, '6HGSS3282390-APZHWP3ND355RY5A');
